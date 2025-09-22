48a930392747f0056c5ea8b0c93ab435c3238edf
HuyenLeCode <lethuhuyenrubik@gmail.com>
2025-09-22 14:42:14 +0700

    Nop BTVN OOP W1

--- HW1/Introduction.java
+++ HW1/Introduction.java
@@ -1,15 +1,14 @@
 public class Introduction {
     public static void main(String[] args) {
-        System.out.println("Le Thu Huyen\t24021522\tCS5\tHuyenLeCode\t24021522@vnu.edu.vn" ) ;
+        System.out.println("Le Thu Huyen\t24021522\tCS5\tHuyenLeCode\t24021522@vnu.edu.vn");
 
-        for ( int i = 9 ; i >= 2 ; i--)
-        {
-            System.out.println( i + " bottles of beer on the wall, " + i + " bottles of beer");
-            System.out.println( "Take one down, pass it around,") ;
+        for (int i = 9; i >= 2; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer");
+            System.out.println("Take one down, pass it around,");
         }
 
-        System.out.println ("1 bottle of beer on the wall, 1 bottle of beer.") ;
-        System.out.println( "Take one down, pass it around,") ;
-        System.out.println( "No more bottles of beer on the wall.");
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
     }
 }
